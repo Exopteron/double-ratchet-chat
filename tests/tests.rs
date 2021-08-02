c8114b09fd0b64c1022b33a8e33f2ecf8ef4b0f9
Exopteron <exopteron@protonmail.com>
2021-08-02 14:19:38 +1200

    More tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -110,5 +110,61 @@ use x25519_dalek::{EphemeralSecret, PublicKey, StaticSecret};
         let mut message = bob.RatchetEncryptHE(b"Hello World".to_vec(), vec![1]);
         let message = alice.RatchetDecryptHE(message.to_vec(), vec![2]).unwrap();
     }
+    #[test]
+    #[should_panic]
+    fn bob_message_first_wrong_sk() {
+        let mut key1 = StaticSecret::new(OsRng);
+        let mut key1public = PublicKey::from(&key1);
+        let mut alice = doubleratchet::StateHE::RatchetInitAliceHE(vec![1; 32], key1public, vec![0; 32], vec![0; 32]);
+        let mut bob = doubleratchet::StateHE::RatchetInitBobHE(vec![0; 32], key1, vec![0; 32], vec![0; 32]);
+        let message = bob.RatchetEncryptHE(b"Hello World".to_vec(), vec![]);
+        let message = alice.RatchetDecryptHE(message.to_vec(), vec![]).unwrap();
+        if message != b"Hello World".to_vec() {
+            panic!("Fail");
+        }
+    }
+    #[test]
+    #[should_panic]
+    fn alice_message_first_wrong_sk() {
+        let mut key1 = StaticSecret::new(OsRng);
+        let mut key1public = PublicKey::from(&key1);
+        let mut alice = doubleratchet::StateHE::RatchetInitAliceHE(vec![1; 32], key1public, vec![0; 32], vec![0; 32]);
+        let mut bob = doubleratchet::StateHE::RatchetInitBobHE(vec![0; 32], key1, vec![0; 32], vec![0; 32]);
+        let message = alice.RatchetEncryptHE(b"Hello World".to_vec(), vec![]);
+        let message = bob.RatchetDecryptHE(message.to_vec(), vec![]).unwrap();
+        if message != b"Hello World".to_vec() {
+            panic!("Fail");
+        }
+    }
+    #[test]
+    #[should_panic]
+    fn alice_message_first_wrong_hka() {
+        let mut key1 = StaticSecret::new(OsRng);
+        let mut key1public = PublicKey::from(&key1);
+        let mut alice = doubleratchet::StateHE::RatchetInitAliceHE(vec![0; 32], key1public, vec![1; 32], vec![0; 32]);
+        let mut bob = doubleratchet::StateHE::RatchetInitBobHE(vec![0; 32], key1, vec![0; 32], vec![0; 32]);
+        let message = alice.RatchetEncryptHE(b"Hello World".to_vec(), vec![]);
+        let message = bob.RatchetDecryptHE(message.to_vec(), vec![]).unwrap();
+        if message != b"Hello World".to_vec() {
+            panic!("Fail");
+        }
+    }
+    #[test]
+    #[should_panic]
+    fn alice_message_herself() {
+        let mut key1 = StaticSecret::new(OsRng);
+        let mut key1public = PublicKey::from(&key1);
+        let mut alice = doubleratchet::StateHE::RatchetInitAliceHE(vec![0; 32], key1public, vec![0; 32], vec![0; 32]);
+        let message = alice.RatchetEncryptHE(b"Hello World".to_vec(), vec![]);
+        let message = alice.RatchetDecryptHE(message.to_vec(), vec![]).unwrap();
+    }
+    #[test]
+    #[should_panic]
+    fn bob_message_himself() {
+        let mut key1 = StaticSecret::new(OsRng);
+        let mut bob = doubleratchet::StateHE::RatchetInitBobHE(vec![0; 32], key1, vec![0; 32], vec![0; 32]);
+        let message = bob.RatchetEncryptHE(b"Hello World".to_vec(), vec![]);
+        let message = bob.RatchetDecryptHE(message.to_vec(), vec![]).unwrap();
+    }
 }
 
